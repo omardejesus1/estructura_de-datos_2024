e07c2c0438a8982bc4e5fd4edaedc8d723c61346
elquesea <2212733@universidad-une.com>
2024-01-23 08:31:48 -0600

    mi primer commit

---
+++ repetitivas.py
@@ -0,0 +1,17 @@
+arr = [1,2,3,4,5]
+for i in arr:
+    if(i% 2 ==0):
+        pass
+    else:
+        print(i)
+d= {} # {'key': 'value', 123: True}
+s= {} # [1,1,2,3,4]=> (1,2,3,4)
+for i in range(0,10,2): # for (int i= 0; i< 10; i +=2)
+    d[i]= i*i
+    print(d)
+    cont= 10
+    while(cont > -5):
+        print(' aún no')
+        cont -= 1
+        if(not cont):
+            break
---
+++ tipos-datos.py
@@ -0,0 +1,6 @@
+x= 1231234
+y= 123.1241
+b= True
+f= False
+a= [1,2,3, False]
+print(x,y,a,sep="***", end="\n\n\n")
